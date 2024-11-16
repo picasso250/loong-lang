adabd81a0bfb23ad41f56cc020db1b9eb4608af0
pica <xi_aochi@163.com>
2024-11-16 22:13:41 +0800

    split 2 files

--- loong.py
+++ loong.py
@@ -1,132 +1,6 @@
-# 解释器 - Loong
-# 该程序实现了一个简单的解释器，能够解析和执行基本的数学表达式、变量赋值、三元运算符、函数定义与调用等功能。
-# 使用 sly 库进行词法分析与语法分析，支持以下功能：
-# - 基本的算术运算：加法、减法、乘法、除法
-# - 比较运算符：大于、小于、等于
-# - 变量赋值与引用
-# - 支持三元运算符
-# - 支持函数定义和调用
 
-# 语法示例：
-# 1. 变量赋值：a := 10
-# 2. 算术表达式：a + b * 2
-# 3. 条件运算符：a > 10 ? "Yes" : "No"
-# 4. 函数定义与调用：
-#    函数定义：f := x => x + 2
-#    函数调用：f 5  # 返回 7
 
-from sly import Lexer, Parser
-
-# 词法分析器
-class CalcLexer(Lexer):
-    tokens = { NAME, NUMBER, ASSIGN, ARROW }
-    ignore = ' \t'
-    literals = { '=', '+', '-', '*', '/', '(', ')', '>', '<', '?', ':', ':=', '=>' }
-
-    NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
-    ASSIGN = ':='
-    ARROW = '=>'
-
-    @_(r'\d+')
-    def NUMBER(self, t):
-        t.value = int(t.value)
-        return t
-
-    @_(r'\n+')
-    def newline(self, t):
-        self.lineno += t.value.count('\n')
-
-    def error(self, t):
-        print("Illegal character '%s'" % t.value[0])
-        self.index += 1
-
-# 语法分析器
-class CalcParser(Parser):
-    tokens = CalcLexer.tokens
-
-    precedence = (
-        ('right', '?', ':'),           # Ternary operator (lowest precedence)
-        ('left', '>', '<', '='),       # Comparison operators
-        ('left', '+', '-'),            # Addition and subtraction
-        ('left', '*', '/'),            # Multiplication and division
-        ('right', 'UMINUS'),           # Unary minus
-    )
-
-    def __init__(self):
-        self.names = {}
-
-    # 语句
-    @_('NAME ASSIGN expr')
-    def statement(self, p):
-        return ('assign', p.NAME, p.expr)
-
-    @_('expr')
-    def statement(self, p):
-        return p.expr
-
-    # 运算符
-    @_('expr "+" expr')
-    def expr(self, p):
-        return ('binop', '+', p.expr0, p.expr1)
-
-    @_('expr "-" expr')
-    def expr(self, p):
-        return ('binop', '-', p.expr0, p.expr1)
-
-    @_('expr "*" expr')
-    def expr(self, p):
-        return ('binop', '*', p.expr0, p.expr1)
-
-    @_('expr "/" expr')
-    def expr(self, p):
-        return ('binop', '/', p.expr0, p.expr1)
-
-    @_('"-" expr %prec UMINUS')
-    def expr(self, p):
-        return ('unaryop', '-', p.expr)
-
-    @_('"(" expr ")"')
-    def expr(self, p):
-        return p.expr
-
-    # 比较运算符
-    @_('expr ">" expr')
-    def expr(self, p):
-        return ('binop', '>', p.expr0, p.expr1)
-
-    @_('expr "<" expr')
-    def expr(self, p):
-        return ('binop', '<', p.expr0, p.expr1)
-
-    @_('expr "=" expr')
-    def expr(self, p):
-        return ('binop', '=', p.expr0, p.expr1)
-
-    # 三元运算符
-    @_('expr "?" expr ":" expr')
-    def expr(self, p):
-        return ('ternary', p.expr0, p.expr1, p.expr2)
-
-    # 数字
-    @_('NUMBER')
-    def expr(self, p):
-        return ('num', p.NUMBER)
-
-    # 变量名
-    @_('NAME')
-    def expr(self, p):
-        return ('name', p.NAME)
-
-    # 函数定义
-    @_('NAME ARROW expr')
-    def expr(self, p):
-        return ('func_def', p.NAME, p.expr)
-
-    # 函数调用
-    @_('expr expr')
-    def expr(self, p):
-        return ('func_call', p.expr0, p.expr1)
-
+from loongpaser import LoongLexer , LoongParser
 # 虚拟机
 class VirtualMachine:
     def __init__(self):
@@ -181,8 +55,8 @@ class VirtualMachine:
             return self.eval(func_name[2])
 
 if __name__ == '__main__':
-    lexer = CalcLexer()
-    parser = CalcParser()
+    lexer = LoongLexer()
+    parser = LoongParser()
     vm = VirtualMachine()
 
     while True:
